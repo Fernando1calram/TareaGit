542a429d4e0d187c4fe84d7df767c3fa8ed7ed45
aemlocal <aemlocal@ED49005994P033.educa.jcyl.es>
2023-04-20 09:26:54 +0200

    Añadidos datos calculadora

--- TareaGit/src/tareaGit/TareaGit.java
+++ TareaGit/src/tareaGit/TareaGit.java
@@ -1,11 +1,25 @@
 package tareaGit;
 
+import java.util.Scanner;
+
 public class TareaGit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola");
-		System.out.println("Adios");
+		Scanner teclado =new Scanner (System.in);
+		
+		System.out.println("Introduce el primer numero");
+		int n1 = teclado.nextInt();
+		
+		System.out.println("Introduce el segundo numero");
+		int n2 = teclado.nextInt();
+		
+		System.out.println(n1 + " + " + n2 + " = " + n1+n2);
+		System.out.println(n1 + " - " + n2 + " = " + (n1-n2));
+		System.out.println(n1 + " * " + n2 + " = " + n1*n2);
+		System.out.println(n1 + " / " + n2 + " = " + n1/n2);
+		
+		teclado.close();
 
 	}
 
